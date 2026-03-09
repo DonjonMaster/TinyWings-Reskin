dde965b47131c5c8708a0db8626866aeb258528d
gkugananthan <gkugananthan@gaming.tech>
2026-03-09 15:43:45 +0100

    test

---
+++ Engine/Constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+
+//INPUTS
+#define SPACE sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)
+#define LMOUSE sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)
+
+
+#endif
