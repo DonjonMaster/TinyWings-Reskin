c6f35d1fbe40de271741d99110440668009731ca
gkugananthan <gkugananthan@gaming.tech>
2026-03-09 15:29:21 +0100

    init des contants INPUTS/PLAYER SETTIINGS + inputHandler

---
+++ Engine/Constants.h
@@ -0,0 +1,17 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+
+//INPUTS
+#define SPACE sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)
+#define LMOUSE sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)
+
+//PLAYER SETTINGS
+//namespace Settings {
+//    const sf::Vector2f DEFAULT_VELOCITY = { 200.f, 0.f };
+//    const float GRAVITY = 20.f;
+//    const float DIVE_FORCE = 500.f;
+//}
+
+
+#endif
---
+++ Engine/InputHandler.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Component.h"
+#include "Constants.h"
+#include "GameObject.h"
+
+class InputHandler : public Component 
+{
+public:
+	void Update(float dt) override
+	{
+	//	if (SPACE || LMOUSE) player.dive();
+	}
+};
